c14f463980470c8bd1f9536e0b667e0296196ea1
Pawan Kumar Gunjan <98067661+PawanKrGunjan@users.noreply.github.com>
2022-02-25 14:36:22 +0530

    Print the range of number from n1 to n2 (n1<n2) having same digits number are not in list like:
    Print the range of number from n1 to n2 (n1<n2) having same digits number are not in list like:
    input n1: 100 , n2 = 250
    range = [100, 111, 112, 113,,,,,,,,,,,,,,,,,,,,220,  221,  222,  223 ,,,,,,,,,,,,,,,,,,,,,,]
    Output =  [100, 112, 113,,,,,,,,,,,,,,,,,,,, 220,  221, 223 ,,,,,,,,,,,,,,,,,,,,,,]

---
+++ TCS2.py
@@ -0,0 +1,32 @@
+n1=int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+#Create a list from n1 to n2
+l=[]
+for i in range(n1,n2):
+    l.append(i)
+print(l)
+
+#Canvert the type of each element of the list to string
+L=[]
+for j in l:
+    L.append(str(j))
+
+#L = [str(x) for x in l]
+#L = list(map(str, l))
+print(L)
+
+# Remove the element having same digits like 11, 22, 33....
+List=[]
+for x in L:
+    i=0
+    while i != len(x):
+        if x[i] == x[0]:
+           pass
+        else:
+            List.append(x)
+        i=i+1
+#ReConvert the each element of output 
+Output = list(map(int, List))
+print(Output)
+
