10c6df6c4928376c25e77f2e2cc42d93a36786b8
Pawan Kumar Gunjan <pawankumargunjan@hotmail.com>
2022-05-24 12:02:20 +0530

    interview

---
+++ Second Largest.py
@@ -0,0 +1,31 @@
+'''
+**input1:** Length of the array
+
+**input2:** An array of positive integers
+
+**Output Specification:**
+Return the second largest number or -1 accordingly.
+'''
+
+input1 = int(input())
+input2 = [int(i) for i in input().strip('{}').split(',')]
+
+
+class UserMainCode(object):
+    def __init__(self, n, m):
+        self.i1 = n
+        self.i2 = m
+
+    @classmethod
+    def secondLargest(self, n, m):
+        m.sort()
+        for i in range(1,n):
+            if m[n - i] != m[n - i - 1]:
+                r = m[n - i - 1]
+                break
+            elif n - i == 1:
+                r =-1
+                break
+            else:
+                pass
+        return r
